2b3fe7855bd250bffdb56653d7c047fb78560e29
Artem Maklashev <artem@maklashev.ru>
2023-10-12 14:08:33 +0400

    hw1 calc edit

--- src/seminars/first/hw/Calculator/Calculator.java
+++ src/seminars/first/hw/Calculator/Calculator.java
@@ -29,16 +29,16 @@ public class Calculator {
 
     // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
     // необходимые проверки для него используя граничные случаи
-    public static double squareRootExtraction(double num) {
-        //  0
-        //  Отрицательные числа
-        //  Дробные значения корней
-        //  Целые
-            if(num < 0) {
-                throw new IllegalArgumentException("Cannot calculate square root of a negative number");
-            }
-            return Math.sqrt(num);
-    }
+//    public static double squareRootExtraction(double num) {
+//        //  0
+//        //  Отрицательные числа
+//        //  Дробные значения корней
+//        //  Целые
+//            if(num < 0) {
+//                throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+//            }
+//            return Math.sqrt(num);
+//    }
 
     // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
     // Примерная сигнатура и тело метода:
